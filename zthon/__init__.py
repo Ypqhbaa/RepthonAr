ac0840b887c1ac0775ac9418642bc4ad91ea5680
روجر <104784913+rogerpq@users.noreply.github.com>
2023-08-28 22:09:19 +0300

    Update __init__.py

--- zthon/__init__.py
+++ zthon/__init__.py
@@ -1,21 +1,17 @@
 import signal
 import sys
 import time
-
 import heroku3
-
 from .Config import Config
 from .core.logger import logging
 from .core.session import zedub
-from .helpers.functions.converter import Convert
-from .helpers.functions.musictool import *
 from .helpers.utils.utils import runasync
 from .sql_helper.globals import addgvar, delgvar, gvarstatus
 
-__version__ = "3.1.3"
+__version__ = "3.1.1"
 __license__ = "GNU Affero General Public License v3.0"
 __author__ = "Repthon <https://github.com/RepthonArabic/RepthonAr>"
-__copyright__ = f"Repthon Copyright (C) 2021 - 2022  {__author__}"
+__copyright__ = f"Repthon Copyright (C) 2020 - 2021  {__author__}"
 
 zedub.version = __version__
 zedub.tgbot.version = __version__
@@ -25,7 +21,17 @@ bot = zedub
 StartTime = time.time()
 repversion = "3.1.3"
 
-if Config.UPSTREAM_REPO == "rep":
+
+def close_connection(*_):
+    print("تم اغلاق اتصال السورس.")
+    runasync(zedub.disconnect())
+    sys.exit(143)
+
+
+signal.signal(signal.SIGTERM, close_connection)
+
+
+if Config.UPSTREAM_REPO == "Rep":
     UPSTREAM_REPO_URL = "https://github.com/RepthonArabic/RepthonAr"
 else:
     UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
@@ -40,7 +46,7 @@ if Config.PRIVATE_GROUP_BOT_API_ID == 0:
         Config.BOTLOG = True
 else:
     if str(Config.PRIVATE_GROUP_BOT_API_ID)[0] != "-":
-        Config.BOTLOG_CHATID = int(f"-" + str(Config.PRIVATE_GROUP_BOT_API_ID))
+        Config.BOTLOG_CHATID = int("-" + str(Config.PRIVATE_GROUP_BOT_API_ID))
     else:
         Config.BOTLOG_CHATID = Config.PRIVATE_GROUP_BOT_API_ID
     Config.BOTLOG = True
@@ -51,7 +57,7 @@ if Config.PM_LOGGER_GROUP_ID == 0:
     else:
         Config.PM_LOGGER_GROUP_ID = int(gvarstatus("PM_LOGGER_GROUP_ID"))
 elif str(Config.PM_LOGGER_GROUP_ID)[0] != "-":
-    Config.PM_LOGGER_GROUP_ID = int(f"-" + str(Config.PM_LOGGER_GROUP_ID))
+    Config.PM_LOGGER_GROUP_ID = int("-" + str(Config.PM_LOGGER_GROUP_ID))
 
 try:
     if Config.HEROKU_API_KEY is not None or Config.HEROKU_APP_NAME is not None:

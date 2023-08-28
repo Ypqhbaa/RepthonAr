c2aa7de063b345a235fbcde448ea76f0689db51d
روجر <104784913+rogerpq@users.noreply.github.com>
2023-08-28 22:13:26 +0300

    Update __main__.py

--- zthon/__main__.py
+++ zthon/__main__.py
@@ -1,16 +1,20 @@
-from telethon.tl.functions.messages import GetMessagesViewsRequest
-import sys, asyncio
+import sys
 import zthon
-from zthon import BOTLOG_CHATID, HEROKU_APP, PM_LOGGER_GROUP_ID
-from telethon import functions
+from zthon import BOTLOG_CHATID, PM_LOGGER_GROUP_ID
 from .Config import Config
 from .core.logger import logging
 from .core.session import zedub
-from .utils import mybot, saves
-from .utils import add_bot_to_logger_group, load_plugins, setup_bot, startupmessage, verifyLoggerGroup
+from .utils import mybot
+from .utils import (
+    add_bot_to_logger_group,
+    load_plugins,
+    saves,
+    setup_bot,
+    startupmessage,
+    verifyLoggerGroup,
+)
 
 LOGS = logging.getLogger("𝐑𝐞𝐩𝐭𝐡𝐨𝐧")
-cmdhr = Config.COMMAND_HAND_LER
 
 print(zthon.__copyright__)
 print(f"المرخصة بموجب شروط  {zthon.__license__}")
@@ -22,13 +26,9 @@ try:
     zedub.loop.run_until_complete(setup_bot())
     LOGS.info("⌭ بـدء تشغيـل البـوت ⌭")
 except Exception as e:
-    LOGS.error(f"{str(e)}")
+    LOGS.error(f"{e}")
     sys.exit()
 
-class CatCheck:
-    def __init__(self):
-        self.sucess = True
-Catcheck = CatCheck()
 
 try:
     LOGS.info("⌭ جـار تفعيـل وضـع الانـلاين ⌭")
@@ -36,7 +36,6 @@ try:
     LOGS.info("✓ تـم تفعيـل الانـلاين .. بـنجـاح ✓")
 except Exception as e:
     LOGS.error(f"- {e}")
-
 
 try:
     LOGS.info("⌭ جـاري تحميـل الملحقـات ⌭")
@@ -46,61 +45,27 @@ except Exception as e:
     LOGS.error(f"- {e}")
 
 
+
 async def startup_process():
-    async def MarkAsViewed(channel_id):
-        from telethon.tl.functions.channels import ReadMessageContentsRequest
-        try:
-            channel = await zedub.get_entity(channel_id)
-            async for message in zedub.iter_messages(entity=channel.id, limit=5):
-                try:
-                    await zedub(GetMessagesViewsRequest(peer=channel.id, id=[message.id], increment=True))
-                except Exception as error:
-                    print ("✅")
-            return True
-
-        except Exception as error:
-            print ("✅")
-
-    async def start_bot():
-      try:
-          List = ["Repthon","Repthon_support","Repthon_help","ZQ_LO","Repthon_vars","Repthon_cklaish","xxfir","eighthon"]
-          from telethon.tl.functions.channels import JoinChannelRequest
-          for id in List :
-              Join = await zedub(JoinChannelRequest(channel=id))
-              MarkAsRead = await MarkAsViewed(id)
-              print (MarkAsRead, "✅")
-          return True
-      except Exception as e:
-        print("✅")
-        return False
-    
     await verifyLoggerGroup()
     await load_plugins("plugins")
     await load_plugins("assistant")
-    print(f"⌔┊تـم تنصيـب ريبـــثون . . بنجـاح ✓ \n⌔┊لـ إظهـار الاوامـر ارسـل (.الاوامر)")
+    print("➖➖➖➖➖ 𝐑𝐞𝐩𝐭𝐡𝐨𝐧™ ➖➖➖➖➖")
+    print("تـم التنصـيب .. بنجـاح ✓")
+    print(
+        f"⌔┊تـم تنصيـب ريبثون يـوزربـوت . . بنجـاح 🧸♥️ \n\n⌔┊تحيـاتي ..  روجر\n⌔┊قنـاة السـورس ↶.\n🌐┊@Repthon"
+    )
+    print("➖➖➖➖➖ 𝐑𝐞𝐩𝐭𝐡𝐨𝐧™ ➖➖➖➖➖")
     await verifyLoggerGroup()
     await add_bot_to_logger_group(BOTLOG_CHATID)
     if PM_LOGGER_GROUP_ID != -100:
         await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
     await startupmessage()
-    Catcheck.sucess = True
-    
-    Checker = await start_bot()
-    if Checker == False:
-        print("#1")
-    else:
-        print ("✅")
-    
     return
 
-
 zedub.loop.run_until_complete(startup_process())
-
 if len(sys.argv) not in (1, 3, 4):
     zedub.disconnect()
-elif not Catcheck.sucess:
-    if HEROKU_APP is not None:
-        HEROKU_APP.restart()
 else:
     try:
         zedub.run_until_disconnected()

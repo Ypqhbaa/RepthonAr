3cdc2d899f6b5a7923047af5a50c73fc78691208
روجر <104784913+rogerpq@users.noreply.github.com>
2023-10-17 13:46:53 +0300

    Update __main__.py

--- zthon/__main__.py
+++ zthon/__main__.py
@@ -1,61 +1,70 @@
+import contextlib
 import sys
+
+from aiohttp import web
+
 import zthon
 from zthon import BOTLOG_CHATID, PM_LOGGER_GROUP_ID
+
 from .Config import Config
 from .core.logger import logging
+from .core.server import web_server
 from .core.session import zedub
-from .utils import mybot
 from .utils import (
     add_bot_to_logger_group,
     load_plugins,
+    mybot,
     saves,
     setup_bot,
     startupmessage,
     verifyLoggerGroup,
 )
 
-LOGS = logging.getLogger("𝐑𝐞𝐩𝐭𝐡𝐨𝐧")
-
-print(zthon.__copyright__)
-print(f"المرخصة بموجب شروط  {zthon.__license__}")
+LOGS = logging.getLogger("𓆩𝐑𝐞𝐩𝐭𝐡𝐨𝐧𓆪")
 
 cmdhr = Config.COMMAND_HAND_LER
 
 try:
-    LOGS.info("⌭ بـدء تنزيـل ريبـــثون ⌭")
+    LOGS.info("يتم بدء البوت المساعد")
     zedub.loop.run_until_complete(setup_bot())
-    LOGS.info("⌭ بـدء تشغيـل البـوت ⌭")
+    LOGS.info("اكتملت عمليه البوت المساعد")
 except Exception as e:
     LOGS.error(f"{e}")
     sys.exit()
 
-
 try:
-    LOGS.info("⌭ جـار تفعيـل وضـع الانـلاين ⌭")
-    zedub.loop.run_until_complete(mybot())
-    LOGS.info("✓ تـم تفعيـل الانـلاين .. بـنجـاح ✓")
-except Exception as e:
-    LOGS.error(f"- {e}")
-
-try:
-    LOGS.info("⌭ جـاري تحميـل الملحقـات ⌭")
+    LOGS.info("يتم تفعيل وضع حمايه الحساب من الاختراق")
     zedub.loop.create_task(saves())
-    LOGS.info("✓ تـم تحميـل الملحقـات .. بنجـاح ✓")
-except Exception as e:
-    LOGS.error(f"- {e}")
+    LOGS.info("تم تفعيل وضع حمايه الحساب من الاختراق")
+except Exception as bb:
+    LOGS.error(f"- {bb}")
+    sys.exit()
 
+
+try:
+    LOGS.info("يتم تفعيل وضع الانلاين")
+    zedub.loop.run_until_complete(mybot())
+    LOGS.info("تم تفعيل وضع الانلاين بنجاح ✓")
+except Exception as meo:
+    LOGS.error(f"- {meo}")
+    sys.exit()
 
 
 async def startup_process():
     await verifyLoggerGroup()
     await load_plugins("plugins")
     await load_plugins("assistant")
-    print("➖➖➖➖➖ 𝐑𝐞𝐩𝐭𝐡𝐨𝐧™ ➖➖➖➖➖")
-    print("تـم التنصـيب .. بنجـاح ✓")
+    print("============================================================")
+    print("تم التنصيب بنجاح على كويب | ســورس ريبـــثون")
     print(
-        f"⌔┊تـم تنصيـب ريبثون يـوزربـوت . . بنجـاح 🧸♥️ \n\n⌔┊تحيـاتي ..  روجر\n⌔┊قنـاة السـورس ↶.\n🌐┊@Repthon"
+        f"لمعرفة اوامر السورس ارسل {cmdhr}الاوامر\
+        \nمجموعة قناة السورس  https://t.me/Repthon_support"
     )
-    print("➖➖➖➖➖ 𝐑𝐞𝐩𝐭𝐡𝐨𝐧™ ➖➖➖➖➖")
+    print("============================================================")
+    app = web.AppRunner(await web_server())
+    await app.setup()
+    bind_address = "0.0.0.0"
+    await web.TCPSite(app, bind_address, Config.PORT).start()
     await verifyLoggerGroup()
     await add_bot_to_logger_group(BOTLOG_CHATID)
     if PM_LOGGER_GROUP_ID != -100:
@@ -63,11 +72,11 @@ async def startup_process():
     await startupmessage()
     return
 
+
 zedub.loop.run_until_complete(startup_process())
-if len(sys.argv) not in (1, 3, 4):
-    zedub.disconnect()
-else:
-    try:
+
+if len(sys.argv) in {1, 3, 4}:
+    with contextlib.suppress(ConnectionError):
         zedub.run_until_disconnected()
-    except ConnectionError:
-        pass
+else:
+    zedub.disconnect()

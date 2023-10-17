3e305e133f17f630a9b7c80a26b7fd0e21c3483c
روجر <104784913+rogerpq@users.noreply.github.com>
2023-10-17 13:49:44 +0300

    Update __init__.py

--- zthon/__init__.py
+++ zthon/__init__.py
@@ -1,21 +1,25 @@
 import signal
 import sys
 import time
+
 import heroku3
+
 from .Config import Config
 from .core.logger import logging
 from .core.session import zedub
+from .helpers.functions.converter import Convert
+from .helpers.functions.musictool import *
 from .helpers.utils.utils import runasync
 from .sql_helper.globals import addgvar, delgvar, gvarstatus
 
-__version__ = "3.1.1"
+__version__ = "3.2.0"
 __license__ = "GNU Affero General Public License v3.0"
-__author__ = "Repthon <https://github.com/RepthonArabic/RepthonAr>"
-__copyright__ = f"Repthon Copyright (C) 2020 - 2021  {__author__}"
+__author__ = "سورس ريبثون <https://github.com/RepthonArabic/RepthonAr/tree/koyeb>"
+__copyright__ = f" حقوق سورس ريبثون (C) 2020 - 2022  {__author__}"
 
 zedub.version = __version__
 zedub.tgbot.version = __version__
-LOGS = logging.getLogger("𝐑𝐞𝐩𝐭𝐡𝐨𝐧")
+LOGS = logging.getLogger("𓆩𝐑𝐞𝐩𝐭𝐡𝐨𝐧𓆪")
 bot = zedub
 
 StartTime = time.time()
@@ -23,18 +27,14 @@ repversion = "3.1.3"
 
 
 def close_connection(*_):
-    print("تم اغلاق اتصال السورس.")
+    print("تم اغلاق الاتصال بالسورس")
     runasync(zedub.disconnect())
     sys.exit(143)
 
 
 signal.signal(signal.SIGTERM, close_connection)
 
-
-if Config.UPSTREAM_REPO == "Rep":
-    UPSTREAM_REPO_URL = "https://github.com/RepthonArabic/RepthonAr"
-else:
-    UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
+UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
 
 if Config.PRIVATE_GROUP_BOT_API_ID == 0:
     if gvarstatus("PRIVATE_GROUP_BOT_API_ID") is None:
@@ -46,7 +46,7 @@ if Config.PRIVATE_GROUP_BOT_API_ID == 0:
         Config.BOTLOG = True
 else:
     if str(Config.PRIVATE_GROUP_BOT_API_ID)[0] != "-":
-        Config.BOTLOG_CHATID = int("-" + str(Config.PRIVATE_GROUP_BOT_API_ID))
+        Config.BOTLOG_CHATID = int(f"-{str(Config.PRIVATE_GROUP_BOT_API_ID)}")
     else:
         Config.BOTLOG_CHATID = Config.PRIVATE_GROUP_BOT_API_ID
     Config.BOTLOG = True
@@ -57,7 +57,7 @@ if Config.PM_LOGGER_GROUP_ID == 0:
     else:
         Config.PM_LOGGER_GROUP_ID = int(gvarstatus("PM_LOGGER_GROUP_ID"))
 elif str(Config.PM_LOGGER_GROUP_ID)[0] != "-":
-    Config.PM_LOGGER_GROUP_ID = int("-" + str(Config.PM_LOGGER_GROUP_ID))
+    Config.PM_LOGGER_GROUP_ID = int(f"-{str(Config.PM_LOGGER_GROUP_ID)}")
 
 try:
     if Config.HEROKU_API_KEY is not None or Config.HEROKU_APP_NAME is not None:
@@ -70,7 +70,7 @@ except Exception:
     HEROKU_APP = None
 
 
-# Global Configiables
+# تعريفات مهمة
 COUNT_MSG = 0
 USERS = {}
 COUNT_PM = {}
@@ -80,11 +80,10 @@ ISAFK = False
 AFKREASON = None
 CMD_LIST = {}
 SUDO_LIST = {}
-# for later purposes
 INT_PLUG = ""
 LOAD_PLUG = {}
 
-# Variables
+# متغيرات
 BOTLOG = Config.BOTLOG
 BOTLOG_CHATID = Config.BOTLOG_CHATID
 PM_LOGGER_GROUP_ID = Config.PM_LOGGER_GROUP_ID

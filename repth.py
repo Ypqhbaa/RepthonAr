491b62f7c92ab75b8acf01e189fec44df143bf73
روجر <104784913+rogerpq@users.noreply.github.com>
2023-10-25 15:14:47 +0300

    Update repth.py

--- repth.py
+++ repth.py
@@ -9,6 +9,6 @@ class Greeting (Resource):
     def get(self):
         return "The Best Of Userbot RepThon"
 
-os.system("python3" "-m" "zthon")
+os.system("python3", "-m", "zthon")
 api.add_resource(Greeting, '/')
 app.run(host="0.0.0.0", port=os.environ.get("PORT", 8080))

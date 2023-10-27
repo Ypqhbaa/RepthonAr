3e608d510b15c2648afb8c799b62b53bf8603ee7
روجر <104784913+rogerpq@users.noreply.github.com>
2023-10-27 13:41:15 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-"import os"
-"from flask import Flask"
-"from flask_restful import Resource , Api"
 echo "-- جـار تـشغـيـل ســورس ريبـــثون --"
-api.add_resource(Greeting, '/')
-app.run(host="0.0.0.0", port=os.environ.get("PORT", 8080))
-os.system("python3 -m zthon")
+python3 -m zthon
